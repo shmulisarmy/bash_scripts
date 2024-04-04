dd2b9d7b15a3b8dc4e8dc02846245d3bf63ae392
shmuli <shmulikeller@gmail.com>
2024-04-04 00:15:54 -0400

    p.sh file now takes up to 2 arguments

--- p.sh
+++ p.sh
@@ -2,8 +2,8 @@
 if [ "$#" -gt 0 ]; then 
     for i in $(seq 1 "$#"); do
         # echo "python3 ${!i}.py"
-        python3 "${!i}.py"
+        python3 "${!i}.py" $1 $2
     done
 else 
-    python3 main.py
+    python3 main.py $1 $2
 fi
